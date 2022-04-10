abb75edf3da377a8b70e1f80313b3b94cf1283f6
YoungMasterGandalf <daniel123chmurny@gmail.com>
2022-04-10 23:02:44 +0200

    Introducing configuration file

---
+++ conf.py
@@ -0,0 +1,12 @@
+#TODO: add configuration for saving data --> mainly fits but maybe also csv, numpy etc.
+
+data_conf = {
+    "path": "./data/test_mag/hmi.m_45s.20101020_000000_TAI.2.magnetogram.fits"
+}
+
+postel_conf = {
+    "origin": (137.51482, 5.5117860),
+    "shape": (1024, 1024),
+    "scale": [0.0301, 0.0301],
+    "make_plot": True
+}
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from dopplergram import Dopplergram
-import datetime
 import numpy as np
 
+from conf import data_conf, postel_conf
+
+file_path = data_conf["path"]
+
+origin = postel_conf["origin"]
+shape = postel_conf["shape"]
+scale = postel_conf["scale"]
+make_plot = postel_conf["make_plot"]
+
 if __name__ == "__main__":
-	start = datetime.datetime.now()
-	file = '../Diplomka/Dopplerograms/hmi.v_45s.20181129_000000_TAI.2.Dopplergram.fits'
+	file = file_path
 
 	dg = Dopplergram(file)
-	data = dg.get_postel_projected_data()
+	data = dg.get_postel_projected_data(origin=origin, shape=shape, scale=scale, make_plot=make_plot)
 
-	np.savetxt("data2.csv", data, delimiter=",")
+	#np.savetxt("data2.csv", data, delimiter=",")
 
-	print("time elapsed ", datetime.datetime.now() - start)
 	print("data from main ", data)
