0e7ff976cb363b76e13496a01fa3ffcc2e4ea033
Daniel Chmúrny <daniel123chmurny@gmail.com>
2022-11-16 13:46:41 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,17 +1,17 @@
 # Input data configuration
-PATH = "./data/test_doppl/hmi.v_45s.20181129_000000_TAI.2.Dopplergram.fits"
-FOLDER_PATH = "./data/test_doppl"
+PATH = "/nfsscratch/chmurnyd/1day/hmi.v_45s.20181129_000000_TAI.2.Dopplergram.fits"
+FOLDER_PATH = "/nfsscratch/chmurnyd/1day"
 
 # Configuration of the Postel projection(s)
-ORIGIN = [250., 30.] # base point for Postel projections' origins
+ORIGIN = [143., -20.] # base point for Postel projections' origins
 SHAPE = [512, 512] # in [px] ... dimension of each projection
 TIME_STEP: float = 45.0 # in [seconds] ... time step between two consecutive files
 SCALE = [0.12, 0.12] # in [deg/px] ... scaling of the Postel projection
-MAKE_PLOT: bool = True
+MAKE_PLOT: bool = False
 R_SUN: float = 696.0 # in [Mm] ... Sun's radius
-ARTIFICIAL_LON_VELOCITY: float = 10.0 # in [m/s] ... origin of each projection will be moved with Sun's rotation and this velocity
+ARTIFICIAL_LON_VELOCITY: float = -170.0 # in [m/s] ... origin of each projection will be moved with Sun's rotation and this velocity
 
 # Output data configuration
-SAVE_FILE: bool = False
-OUTPUT_DIR = "."
-FILENAME = "test_file.fits"
+SAVE_FILE: bool = True
+OUTPUT_DIR = "/nfsscratch/chmurnyd/1day/datacubes"
+FILENAME = "new_quadr_surf_corr.fits"
