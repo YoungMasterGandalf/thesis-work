39a3bd7958fcb1c223d7b5365b426228bdef5239
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-03-17 17:09:41 +0100

    Modify activate_conda.sh

--- activate_conda.sh
+++ activate_conda.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-eval "$(/nfshome/ANACONDA3/bin/conda shell.bash hook)"
+echo "Running hook ..."
+eval "$(/nfshome/ANACONDA3/bin/conda shell.bash hook)" &
 wait
+echo "Activating conda environment ..." &
 conda activate py39
