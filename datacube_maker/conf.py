9b2bd71a24db6571f93db74247644edf55db48de
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-04-26 21:13:55 +0200

    Move main datacube configuration to conf.json

--- datacube_maker/conf.py
+++ datacube_maker/conf.py
@@ -1,24 +1,36 @@
+"""
+Auxiliary configuration file, do not change the configuration manually here! 
+All configuration changes should be done in conf.json only!
+
+This file serves mainly as a storage for comments of various parameters (might be deprecated in the future).
+"""
+
+import json
+
+with open("conf.json", 'r') as file:
+   conf = json.load(file)
+
 # Input data configuration
-PATH = "/nfsscratch/chmurnyd/1day/hmi.v_45s.20181129_000000_TAI.2.Dopplergram.fits"
-FOLDER_PATH = "/nfsscratch/chmurnyd/1day"
+PATH = conf["path"]
+FOLDER_PATH = conf["file_path"]
 
 # Configuration of the Postel projection(s)
-TEST_MODE = False # if True: creates only one projection and plots it --> used for testing purposes (that all is working as expected, coordinates are reasonable etc.)
+TEST_MODE = conf["test_mode"] # if True: creates only one projection and plots it --> used for testing purposes (that all is working as expected, coordinates are reasonable etc.)
 
-ORIGIN = [70., -20.] # base point for Postel projections' origins
-SHAPE = [512, 512] # in [px] ... dimension of each projection
-TIME_STEP: float = 45.0 # in [seconds] ... time step between two consecutive files
-SCALE = [0.12, 0.12] # in [deg/px] ... scaling of the Postel projection
-R_SUN: float = 696.0 # in [Mm] ... Sun's radius
-ARTIFICIAL_LON_VELOCITY: float = -170.0 # in [m/s] ... origin of each projection will be moved with Sun's rotation and this velocity
+ORIGIN = conf["origin"] # base point for Postel projections' origins
+SHAPE = conf["shape"] # in [px] ... dimension of each projection
+TIME_STEP: float = conf["time_step"] # in [seconds] ... time step between two consecutive files
+SCALE = conf["scale"] # in [deg/px] ... scaling of the Postel projection
+R_SUN: float = conf["r_sun"] # in [Mm] ... Sun's radius
+ARTIFICIAL_LON_VELOCITY: float = conf["artificial_lon_velocity"] # in [m/s] ... origin of each projection will be moved with Sun's rotation and this velocity
 
 # Output data configuration
-OUTPUT_DIR = "/Users/daniel/Downloads"
-FILENAME = "new_quadr_surf_corr.fits"
+OUTPUT_DIR = conf["output_dir"]
+FILENAME = conf["filename"]
 
-# DRMS conf (TODO: maybe find a better place to store this into, e.g. .yaml file)
-RUN_VIA_DRMS = True # if False --> takes files locally
-JSOC_EMAIL = "daniel123chmurny@gmail.com"
-DOPPL_REQUEST = "hmi.v_45s[2011.01.11_00:00:00_TAI/1h]{Dopplergram}"
-DRMS_FILES_PATH = "/Users/daniel/Downloads/drms_temp_files"
-DELETE_FILES_WHEN_FINISHED = True # After the datacube is created: True ==> deletes downloaded .fits files (can preserve disk storage)
+# DRMS conf 
+RUN_VIA_DRMS = conf["run_via_drms"] # if False --> takes files locally
+JSOC_EMAIL = conf["jsoc_email"]
+DOPPL_REQUEST = conf["doppl_request"]
+DRMS_FILES_PATH = conf["drms_files_path"]
+DELETE_FILES_WHEN_FINISHED = conf["delete_files_when_finished"] # After the datacube is created: True ==> deletes downloaded .fits files (can preserve disk storage)
