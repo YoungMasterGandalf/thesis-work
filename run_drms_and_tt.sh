d1a7fef8e801841630adc290dee98e02af0be7e5
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-09-14 11:56:41 +0200

    Change running to ra-local only

--- run_drms_and_tt.sh
+++ run_drms_and_tt.sh
@@ -11,10 +11,19 @@ tt_wd=$6
 tt_conffile=$7
 
 cd $drms_path
-# drms_output=`qsub -l nodes=radegast-local -v WD=$drms_wd,LD=$drms_ld,CONFFILE=$drms_conffile drms_datacube.pbs`
-drms_output=`qsub -v WD=$drms_wd,LD=$drms_ld,CONFFILE=$drms_conffile drms_datacube.pbs`
+
+## Run on radegast-local only --> should solve some issues with passing logs
+drms_output=`qsub -l nodes=radegast-local -v WD=$drms_wd,LD=$drms_ld,CONFFILE=$drms_conffile drms_datacube.pbs`
+
+## Run on any core --> better not use this
+# drms_output=`qsub -v WD=$drms_wd,LD=$drms_ld,CONFFILE=$drms_conffile drms_datacube.pbs`
+
 echo $drms_output
 
 cd $tt_path
-tt_output=`qsub -W depend=afterok:$drms_output -v WD=$tt_wd,CONFFILE=$tt_conffile run_tt_pipeline.pbs`
+## Run on radegast-local only --> should solve some issues with passing logs
+tt_output=`qsub -l nodes=radegast-local -W depend=afterok:$drms_output -v WD=$tt_wd,CONFFILE=$tt_conffile run_tt_pipeline.pbs`
+
+## Run on any core --> better not use this
+# tt_output=`qsub -W depend=afterok:$drms_output -v WD=$tt_wd,CONFFILE=$tt_conffile run_tt_pipeline.pbs`
 echo $tt_output
