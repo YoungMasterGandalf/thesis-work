518e82eb59052d44feffa56f4b1d2d61a9263679
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-09-30 22:03:28 +0200

    Refactor datacube_pipeline_prerequisitor.py

--- datacube_pipeline_prerequisitor.py
+++ datacube_pipeline_prerequisitor.py
@@ -39,6 +39,82 @@ PARAM_EXAMPLE_CONF_PATH: str = os.path.join(TRAVEL_TIMES_ROOT_FOLDER, "PARAM-EXA
 
 REQUESTS_FILE_PATH = "/nfshome/chmurnyd/GitHub/thesis-work/datacube_pipeline_helper_files/requests_ready_for_download.json"
 
+def create_data_directory_from_request(request: str) -> str:
+    request_name = create_request_name_from_request_string(request=request)
+    data_folder_name = f'{request_name}_data'
+    data_path = os.path.join(OUTPUT_ROOT_FOLDER, data_folder_name)
+    print(f'Creating data directory for request {request}:\n"{data_path}"...')
+    os.makedirs(data_path)
+    
+    return data_path
+
+def create_datacube_directory(request: str, origin: list[float], velocity: float) -> str:
+    datacube_dir_name = create_datacube_directory_name(request, origin, velocity)
+    datacube_dir_path = os.path.join(OUTPUT_ROOT_FOLDER, datacube_dir_name)                
+    print(f'Creating datacube directory: "{datacube_dir_path}"...')
+    os.makedirs(datacube_dir_path)
+    
+    return datacube_dir_name, datacube_dir_path
+
+def create_datacube_logs_directory(datacube_dir_path: str) -> str:
+    logs_path = os.path.join(datacube_dir_path, "logs")
+    print(f'Creating logs directory: "{logs_path}"...')
+    os.makedirs(logs_path)
+    
+    return logs_path
+
+def copy_and_fill_in_conf_json(datacube_dir_path: str, data_path: str, origin: list[float], velocity: float, datacube_dir_name: str) -> str:
+    with open("datacube_maker/conf.json", "r") as file:
+        conf_dict = json.load(file)
+        
+    conf_dict["folder_path"] = data_path
+    conf_dict["test_mode"] = TEST_MODE
+    conf_dict["origin"] = origin
+    conf_dict["shape"] = SHAPE
+    conf_dict["time_step"] = TIME_STEP
+    conf_dict["scale"] = SCALE
+    conf_dict["r_sun"] = R_SUN
+    conf_dict["artificial_lon_velocity"] = velocity
+    conf_dict["output_dir"] = datacube_dir_path
+    conf_dict["filename"] = f'{datacube_dir_name}.fits'
+    
+    conf_file_path = os.path.join(datacube_dir_path, "conf.json")
+    print(f'Storing configuration into "{conf_file_path}"...')
+    with open(conf_file_path, "w") as file:
+        json.dump(conf_dict, file, indent=4)
+        
+    return conf_file_path
+        
+def copy_traveltime_conf_file_to_new_path(traveltime_conf_file_name: str) -> str:
+    new_travel_time_conf_path = os.path.join(TRAVEL_TIMES_ROOT_FOLDER, traveltime_conf_file_name)
+    print(f'New TT conf file path: {new_travel_time_conf_path}')
+    shutil.copyfile(PARAM_EXAMPLE_CONF_PATH, new_travel_time_conf_path)
+    
+    return new_travel_time_conf_path
+
+def update_datacube_path_and_traveltime_outdir_in_new_travel_time_conf(new_travel_time_conf_path: str, datacube_path: str,
+                                                                       travel_time_outdir: str):
+    with open(new_travel_time_conf_path, 'r') as file:
+        conf_file_lines = file.readlines()
+        
+    is_query_changed = False
+    is_outdir_changed = False
+    for i, line in enumerate(conf_file_lines):
+        if line.startswith("p.query"):
+            conf_file_lines[i] = f"p.query='{datacube_path}';"
+            is_query_changed = True
+            
+        if line.startswith("p.outdir"):
+            conf_file_lines[i] = f"p.outdir='./{travel_time_outdir}/';"
+            is_outdir_changed = True
+            
+        if is_query_changed and is_outdir_changed:
+            break
+        
+    with open(new_travel_time_conf_path, 'w') as file:
+        text = "\n".join(conf_file_lines)
+        file.write(text)
+
 def create_folder_structure(origins:list[list[float]], velocities:list[float]):
     datacube_maker_inputs: list[dict] = []
     request_output_dir_dict: dict[str, str] = {}
@@ -46,11 +122,7 @@ def create_folder_structure(origins:list[list[float]], velocities:list[float]):
     # for i, request in enumerate(DRMS_REQUESTS):
     for request, data_path in DRMS_REQUESTS.items():
         if data_path is None:
-            request_name = create_request_name_from_request_string(request=request)
-            data_folder_name = f'{request_name}_data'
-            data_path = os.path.join(OUTPUT_ROOT_FOLDER, data_folder_name)
-            print(f'Creating data directory for request {request}:\n"{data_path}"...')
-            os.makedirs(data_path)
+            data_path = create_data_directory_from_request(request=request)
             
             request_output_dir_dict[request] = data_path
         
@@ -59,67 +131,21 @@ def create_folder_structure(origins:list[list[float]], velocities:list[float]):
                 print(f'Origin {j}: {origin}')
                 print(f'Velocity {k}: {velocity}')
                 
-                datacube_dir_name = create_datacube_directory_name(request, origin, velocity)
-                datacube_dir_path = os.path.join(OUTPUT_ROOT_FOLDER, datacube_dir_name)                
-                print(f'Creating datacube directory: "{datacube_dir_path}"...')
-                os.makedirs(datacube_dir_path)
+                datacube_dir_name, datacube_dir_path = create_datacube_directory(request=request, origin=origin, velocity=velocity)
                 
-                drms_temp_path = os.path.join(datacube_dir_path, "drms_temp_files")
+                logs_path = create_datacube_logs_directory(datacube_dir_path=datacube_dir_path)
                 
-                logs_path = os.path.join(datacube_dir_path, "logs")
-                print(f'Creating logs directory: "{logs_path}"...')
-                os.makedirs(logs_path)
-                logs_path = logs_path.replace("/nfsscratch/chmurnyd/", "") # TODO: just a hotfix --> get rid of this ASAP
-                
-                with open("datacube_maker/conf.json", "r") as file:
-                    conf_dict = json.load(file)
-                    
-                conf_dict["folder_path"] = data_path
-                conf_dict["test_mode"] = TEST_MODE
-                conf_dict["origin"] = origin
-                conf_dict["shape"] = SHAPE
-                conf_dict["time_step"] = TIME_STEP
-                conf_dict["scale"] = SCALE
-                conf_dict["r_sun"] = R_SUN
-                conf_dict["artificial_lon_velocity"] = velocity
-                conf_dict["output_dir"] = datacube_dir_path
-                conf_dict["filename"] = f'{datacube_dir_name}.fits'
-                
-                conf_file_path = os.path.join(datacube_dir_path, "conf.json")
-                print(f'Storing configuration into "{conf_file_path}"...')
-                with open(conf_file_path, "w") as file:
-                    json.dump(conf_dict, file, indent=4)
+                conf_file_path = copy_and_fill_in_conf_json(datacube_dir_path, data_path, origin, velocity, datacube_dir_name)
                     
                 traveltime_conf_file_name = f'TT_{datacube_dir_name}.conf'
-                new_travel_time_conf_path = os.path.join(TRAVEL_TIMES_ROOT_FOLDER, traveltime_conf_file_name)
-                print(f'New TT conf file path: {new_travel_time_conf_path}')
-                shutil.copyfile(PARAM_EXAMPLE_CONF_PATH, new_travel_time_conf_path)
-                
-                with open(new_travel_time_conf_path, 'r') as file:
-                    conf_file_lines = file.readlines()
+                new_travel_time_conf_path = copy_traveltime_conf_file_to_new_path(traveltime_conf_file_name)
                     
                 datacube_path = os.path.join(datacube_dir_path, f'{datacube_dir_name}.fits')
                 travel_time_outdir = f'TT_{datacube_dir_name}'
                 travel_time_outdir_path = os.path.join(TRAVEL_TIMES_ROOT_FOLDER, travel_time_outdir)
                 os.makedirs(travel_time_outdir_path)
-                    
-                is_query_changed = False
-                is_outdir_changed = False
-                for i, line in enumerate(conf_file_lines):
-                    if line.startswith("p.query"):
-                        conf_file_lines[i] = f"p.query='{datacube_path}';"
-                        is_query_changed = True
-                        
-                    if line.startswith("p.outdir"):
-                        conf_file_lines[i] = f"p.outdir='./{travel_time_outdir}/';"
-                        is_outdir_changed = True
-                        
-                    if is_query_changed and is_outdir_changed:
-                        break
-                    
-                with open(new_travel_time_conf_path, 'w') as file:
-                    text = "\n".join(conf_file_lines)
-                    file.write(text)
+                
+                update_datacube_path_and_traveltime_outdir_in_new_travel_time_conf(new_travel_time_conf_path, datacube_path, travel_time_outdir)
                     
                 datacube_maker_input = {
                     "working_dir": os.path.join(os.getcwd(), "datacube_maker"),
@@ -138,7 +164,8 @@ if __name__ == "__main__":
         os.makedirs(OUTPUT_ROOT_FOLDER)
 
     origins = list(itertools.product(LONGITUDES, LATITUDES))
-    velocities = np.random.uniform(low=LOWER_VELOCITY_LIMIT, high=UPPER_VELOCITY_LIMIT, size=VELOCITY_SAMPLE_COUNT)
+    # velocities = np.random.uniform(low=LOWER_VELOCITY_LIMIT, high=UPPER_VELOCITY_LIMIT, size=VELOCITY_SAMPLE_COUNT)
+    velocities = np.floor(np.linspace(start=LOWER_VELOCITY_LIMIT, stop=UPPER_VELOCITY_LIMIT, num=VELOCITY_SAMPLE_COUNT))
     datacube_maker_inputs, request_output_dir_dict = create_folder_structure(origins, velocities)
     
     with open(REQUESTS_FILE_PATH, "w") as file:
