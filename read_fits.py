a48b12e41ba2af807c97c86dd26f7a69eac334ca
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-09-30 22:53:05 +0200

    Delete read_fits.py

--- read_fits.py
+++
@@ -1,9 +0,0 @@
-from astropy.io import fits
-
-hdul = fits.open("/nfshome/michal/students/Chmurny/DATACUBES/new_quad_surf_corr.fits")
-# hdul = fits.open("/home/daniel/Downloads/new_quadr_surf_corr.fits")
-
-print(hdul.info())
-print(hdul[0].header)
-
-hdul.close()
