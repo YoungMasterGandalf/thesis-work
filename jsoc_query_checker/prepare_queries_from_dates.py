a337fecd09e5e188e3e15c0e8792eba08aba1168
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-10-24 13:24:19 +0200

    Fix datetime handling in prepare queries from dates

--- jsoc_query_checker/prepare_queries_from_dates.py
+++ jsoc_query_checker/prepare_queries_from_dates.py
@@ -3,10 +3,15 @@ import datetime
 
 def create_a_one_day_dopplergram_query_from_date(date: datetime.datetime) -> str:
     year = date.year
-    month = date.month
-    day = date.day
+    month = date.strftime('%m')
+    day = date.strftime('%d')
     
-    jsoc_query = f'hmi.v_45s[{year}.{month}.{day}_00:00:00_TAI-{year}.{month}.{day + 1}_00:00:00_TAI]{{Dopplergram}}'
+    following_date = date + datetime.timedelta(days=1)
+    following_date_year = following_date.year
+    following_date_month = following_date.strftime('%m')
+    following_date_day = following_date.strftime('%d')
+    
+    jsoc_query = f'hmi.v_45s[{year}.{month}.{day}_00:00:00_TAI-{following_date_year}.{following_date_month}.{following_date_day}_00:00:00_TAI]{{Dopplergram}}'
     
     return jsoc_query
 
@@ -22,6 +27,7 @@ if __name__ == "__main__":
     queries: list[str] = []
         
     for date_string in dates:
+        date_string = date_string.strip() # Strip '\n' and trailing spaces
         date = datetime.datetime.strptime(date_string, "%Y%m%d")
         jsoc_query = create_a_one_day_dopplergram_query_from_date(date=date)
         queries.append(jsoc_query)
