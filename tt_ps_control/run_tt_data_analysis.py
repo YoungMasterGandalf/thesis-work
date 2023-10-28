31d800a132e72a5b8a4873df1f7ef3e7a45b217b
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-10-28 16:55:49 +0200

    Add delay to qsub analysis

--- tt_ps_control/run_tt_data_analysis.py
+++ tt_ps_control/run_tt_data_analysis.py
@@ -2,6 +2,7 @@ import os
 import sys
 import re
 import subprocess
+import time
 
 PATTERN: str = "TT_hmi\.v_45s_(\d{4})\.(\d{2})\.(\d{2})_00\.00\.00_lon_(plus|minus)_(\d+)_lat_(plus|minus)_(\d+)_vel_(plus|minus)_(\d+)"
 RUN_VIA_QSUB: bool = False
@@ -20,12 +21,13 @@ def main(folder_path, pattern):
     # Find folders containing the pattern and pass each as an argument to the python script
     for folder in os.listdir("."):
         if os.path.isdir(folder) and regex_pattern.match(folder):
+            print(f"Analyzing folder {folder}...")
             if RUN_VIA_QSUB:
                 pbs_file_path = os.path.join(python_script_dir, "run_analyze_tt_results.pbs")
                 subprocess.run(["qsub", "-v", f'python_script_path={python_script_path},data_folder={folder}', pbs_file_path])
+                time.sleep(1) # Add some delay so qsub doesn't get overwhelmed
             else:
                 subprocess.run(["python", python_script_path, folder])
-            print(f"Finished analyzing {folder}.")
 
     print("Finished analyzing folders.")
 
