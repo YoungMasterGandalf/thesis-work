a3f970b4e7722123d737b2a7a92e58140390ebab
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-10-28 16:40:54 +0200

    Improve run data analysis file

--- tt_ps_control/run_tt_data_analysis.py
+++ tt_ps_control/run_tt_data_analysis.py
@@ -3,10 +3,13 @@ import sys
 import re
 import subprocess
 
+PATTERN: str = "TT_hmi\.v_45s_(\d{4})\.(\d{2})\.(\d{2})_00\.00\.00_lon_(plus|minus)_(\d+)_lat_(plus|minus)_(\d+)_vel_(plus|minus)_(\d+)"
+RUN_VIA_QSUB: bool = False
+
 def main(folder_path, pattern):
     # Store current working dir (containing the python script for data analysis) to a variable
     python_script_dir = os.getcwd()
-    python_script_path = os.path.join(python_script_dir, "analyze_f_and_p_mode_results.py")
+    python_script_path = os.path.join(python_script_dir, "analyze_tt_results.py")
 
     # Go to the folder
     os.chdir(folder_path)
@@ -19,15 +22,13 @@ def main(folder_path, pattern):
         if os.path.isdir(folder) and regex_pattern.match(folder):
             subprocess.run(["python", python_script_path, folder])
             print(f"Finished analyzing {folder}.")
-            # subprocess.run(["sleep", "3"])
 
     print("Finished analyzing folders.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <folder_path> <pattern>")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <folder_path>")
         sys.exit(1)
 
     folder_path = sys.argv[1]
-    pattern = sys.argv[2]
-    main(folder_path, pattern)
+    main(folder_path, PATTERN)
