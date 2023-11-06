780b67c916e1b35eb201604a6171344f73327702
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-11-06 11:07:06 +0100

    Update run_tt_data_analysis.py

--- tt_ps_control/run_tt_data_analysis.py
+++ tt_ps_control/run_tt_data_analysis.py
@@ -33,7 +33,7 @@ def main(folder_path, pattern):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <folder_path>")
+        print("Usage: python run_tt_data_analysis.py <folder_path>")
         sys.exit(1)
 
     folder_path = sys.argv[1]
