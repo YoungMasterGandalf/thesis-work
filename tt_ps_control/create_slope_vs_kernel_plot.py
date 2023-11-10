baea06b223fba016fbb26263066ab4d2347f7d27
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-11-10 23:58:47 +0100

    Create create_slope_vs_kernel_plot.py

---
+++ tt_ps_control/create_slope_vs_kernel_plot.py
@@ -0,0 +1,82 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from typing import Union
+from scipy.stats import linregress
+
+import test_traveltimes as tt
+
+from matlab_file_reading import read_mat_file
+
+DATA_FILE_PATH = "/nfshome/chmurnyd/plots-2023-11-09/slopes_and_intercepts.csv"
+KERNEL_ROOT_DIR = "/seismo2/michal/FLOW_KERNELS/PtA"
+OUTPUT_FILE_PATH = "/nfshome/chmurnyd/slope_vs_kernel.png"
+
+def create_kernel_filename(mode: str, geometry: str, distance: Union[int, float]):
+    filename = f'k_ann_{mode}_kx_{geometry}_{distance}.mat'
+    
+    return filename
+
+def get_integral_from_kernel_file(root_dir: str, filename: str):
+    
+    kernel_file_path = os.path.join(root_dir, filename)
+    
+    mat_file = read_mat_file(kernel_file_path)
+    integral = mat_file["integral"]
+    integral = tt.convert_data_from_h5py_dataset_to_numpy_array_if_needed(integral)
+    integral = tt.flatten_array_and_get_first_value(integral)
+    
+    return integral
+
+if __name__ == "__main__":
+    if not os.path.exists(DATA_FILE_PATH):
+        raise FileNotFoundError(f'File {DATA_FILE_PATH} does not exist.')
+    
+    slopes = []
+    integrals = []
+    
+    df = pd.read_csv(DATA_FILE_PATH)
+    
+    for i, row in df.iterrows():
+        slope = row["slope"] * 10**3 # To match kernel units
+        kernel_filename = create_kernel_filename(mode=row["mode"], geometry=row["geometry"], distance=row["distance"])
+        integral = get_integral_from_kernel_file(root_dir=KERNEL_ROOT_DIR, filename=kernel_filename)
+        
+        slopes.append(row["slope"])
+        integrals.append(integral)
+        
+    # Fit data with linear regression
+    fit_slope, fit_intercept, r_value, p_value, std_err = linregress(integrals, slopes)
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    
+    ax.scatter(integrals, slopes, label="Linear regressions' slopes vs. kernels")
+
+    line = np.poly1d([fit_slope, fit_intercept])
+    plt.plot(integrals, line(integrals), color='red', label=f'Linear fit (y = {fit_slope}x + {fit_intercept})')
+    
+    ax.set_title("Kernel integrals vs. linear regressions slopes")
+
+    # Labels
+    ax.set_xlabel(r'Kernel integral', fontsize=14)
+    ax.set_ylabel(r'Linear regression slope', fontsize=14)
+
+    # Legend
+    ax.legend()
+
+    # Ticks
+    ax.tick_params(axis='both', which='major', labelsize=10)
+    ax.minorticks_on()
+    ax.tick_params(axis='both', which='minor', labelsize=8)
+
+    # Gridlines for scientific plot design
+    ax.grid(which='major', linestyle='-', linewidth='0.5', color='black')
+    ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+
+    # Save plot
+    plt.savefig(OUTPUT_FILE_PATH, dpi=600)
+    
+    # Close plot to release memory
+    plt.close(fig)
