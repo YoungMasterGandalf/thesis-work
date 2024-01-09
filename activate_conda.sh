e2957eda860ed8a0948626204d87596d8d2b7998
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-01-09 21:47:01 +0100

    Change conda shell script to show hook

--- activate_conda.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo "Running hook ..."
-eval "$(/software/anaconda3/bin/conda shell.bash hook)"
-echo "Activating conda environment ..."
---
+++ show_conda_hook.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+echo "To activate conda environment on cluster, run:"
+echo 'eval "$(/software/anaconda3/bin/conda shell.bash hook)"'
