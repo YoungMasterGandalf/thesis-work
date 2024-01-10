ebb0786f30bf59d0d4e10088894ccd38bb9cbf48
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-01-10 22:49:35 +0100

    Refactor plot_ring_diagram function

--- tt_ps_control/plot_powerspectrum.py
+++ tt_ps_control/plot_powerspectrum.py
@@ -9,30 +9,32 @@ def plot_ring_diagram(mat_file):
     kx = mat_file["kx"]
     ky = mat_file["ky"]
     
-    #* x axis: k (Mm^-1)
-    x_axis_extent = [0, np.pi] #? Is this extent right? Shouldn't it be dependent on the data/keys?
+    kx_axis_extent = [kx.min(), kx.max()]
+    ky_axis_extent = [ky.min(), ky.max()]
     
-    #* y axis: nu (mHz)
-    # y_axis_extent_omega = [mat_file["omega"].min(), mat_file["omega"].max()]
-    # y_axis_extent = [element/(2*np.pi)*10**3 for element in y_axis_extent_omega] #* omega (Hz) --> nu (mHz) conversion
-    
-    for i in range(5):
+    for i, cut_omega in enumerate(mat_file["freq"]):
+        cut_omega = cut_omega[0]
+        
+        # convert angular frequency (omega) in Hz --> frequncy (nu) in mHz
+        cut_frequency = cut_omega / (2 * np.pi) * 1000
+        
         data = mat_file["kx_ky"][:, :, i]
-        data = np.fft.fftshift(data)
-        cut_omega = mat_file["freq"][i][0]
-        cut_frequency = cut_omega/(2 * np.pi) * 1000 # mHz
+        data = np.fft.fftshift(data) # Fourier shift --> zero frequency component to the center
+        
         fig = plt.figure()
-        # plt.imshow(k_omega, cmap='jet', norm=colors.LogNorm(), extent=x_axis_extent + y_axis_extent, aspect='auto')
-        plt.imshow(data, cmap='jet', norm=colors.LogNorm(), 
-                   extent=[kx.min(), kx.max()] + [ky.min(), ky.max()])
+        
+        plt.imshow(data, cmap='jet', norm=colors.LogNorm(), extent=kx_axis_extent + ky_axis_extent)
+        
+        # Plot control lines crossing zero
+        plt.plot(kx_axis_extent, [0, 0], "k--", lw=0.5) # kx = const = 0 line
+        plt.plot([0, 0], [ky.min(), ky.max()], "k--", lw=0.5) # ky = const = 0 line
+        
         plt.title(r'Ring diagram ($\nu$ = ' + str(cut_frequency) + ' mHz)')
-        # plt.xlabel(r'$k$ (Mm$^{-1}$)')
-        # plt.ylabel(r'$\nu$ (mHz)')
-        plt.plot([kx.min(), kx.max()], [0, 0], "k--", lw=0.5)
-        plt.plot([0, 0], [ky.min(), ky.max()], "k--", lw=0.5)
         plt.xlabel(r'$k_x$ (Mm$^{-1}$)')
         plt.ylabel(r'$k_y$ (Mm$^{-1}$)')
+        
         plt.grid(False)
+        
     # plt.savefig(save_to, dpi=300)
     plt.show()
     
