ee1cd9498fe622e670baf4278bfec39f234f95ab
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-02-03 15:01:55 +0100

    Disable the grid

--- helio/data_analysis/create_slope_vs_kernel_plot.py
+++ helio/data_analysis/create_slope_vs_kernel_plot.py
@@ -100,8 +100,8 @@ if __name__ == "__main__":
     ax.tick_params(axis='both', which='minor', labelsize=8)
 
     # Gridlines for scientific plot design
-    ax.grid(which='major', linestyle='-', linewidth='0.5', color='black')
-    ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    # ax.grid(which='major', linestyle='-', linewidth='0.5', color='black')
+    # ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 
     # Save plot
     plt.savefig(OUTPUT_FILE_PATH, dpi=600)
